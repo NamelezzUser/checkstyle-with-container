201d2004c154dddf9cad6e98f09789d9ef76aa36
Enes Korkmaz <enkorkmaz@stud.hs-heilbronn.de>
2024-10-11 16:08:28 +0200

    naming + good & bad example

--- BadExample.java
+++ BadExample.java
@@ -1,46 +1,35 @@
-// Import necessary packages
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * A simple class to demonstrate Checkstyle compliance.
- */
-public class CheckstyleExample {
+public class BadExample {
     
     private List<String> names;
 
     /**
      * Constructor initializes the names list.
      */
-    public CheckstyleExample() {
+    public CheckstyleExample()   {
         names = new ArrayList<>();
     }
 
     /**
      * Adds a name to the list.
      * 
-     * @param name the name to be added
+     * @param name
      */
-    public void addName(String name) {
-        if (name != null && !name.isEmpty()) {
-            names.add(name);
+    public void addName(String n) {
+        if (n != null && !n.isEmpty()) {
+            names.add(n);
         }
     }
 
     /**
-     * Gets the list of names.
-     * 
-     * @return a list of names
+     * Gets the list of names
      */
     public List<String> getNames() {
         return new ArrayList<>(names);
     }
 
-    /**
-     * Main method to run the example.
-     * 
-     * @param args command line arguments
-     */
     public static void main(String[] args) {
         CheckstyleExample example = new CheckstyleExample();
         example.addName("Alice");
